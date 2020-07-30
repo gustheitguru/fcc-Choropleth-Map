677e87107a8d9285f29a4c3213fea08f621f146c
Gus <gusr@logicliving.com>
2020-07-29 20:27:29 -0500

    final project

--- script.js
+++ script.js
@@ -1,77 +1,87 @@
-// console.log(d3)
-// console.log(topojson)
 
-let educationURL =  'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
 let countyURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
+let educationURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
 
 let countyData
 let educationData
 
-let canvas = d3.selection('#canvas')
+let canvas = d3.select('#canvas')
+let tooltip = d3.select('#tooltip')
 
 let drawMap = () => {
+ 
+    canvas.selectAll('path')
+            .data(countyData)
+            .enter()
+            .append('path')
+            .attr('d', d3.geoPath())
+            .attr('class', 'county')
+            .attr('fill', (countyDataItem) => {
+                let id = countyDataItem['id']
+                let county = educationData.find((item) => {
+                    return item['fips'] === id
+                })
+                let percentage = county['bachelorsOrHigher']
+                if(percentage <= 15){
+                    return 'red'
+                }else if(percentage <= 30){
+                    return 'blue'
+                }else if(percentage <= 45){
+                    return 'green'
+                }else{
+                    return 'gold'
+                }
+            })
+            .attr('data-fips', (countyDataItem) => {
+                return countyDataItem['id']
+            })
+            .attr('data-education', (countyDataItem) => {
+                let id = countyDataItem['id']
+                let county = educationData.find((item) => {
+                    return item['fips'] === id
+                })
+                let percentage = county['bachelorsOrHigher']
+                return percentage
+            })
+            .on('mouseover', (countyDataItem)=> {
+                tooltip.transition()
+                    .style('visibility', 'visible')
 
-	canvas.selectAll('path')
-		  .data(countyData)
-		  .enter()
-		  .append('path')
-		  .attr('d', d3.geoPath())
-		  .attr('class', 'county')
-		  .attr('fill', (item) => {
-		  	let fips = item['id']
-		  	let county = educationData.find((county) => {
-		  		return county['fips'] === fips
-		  	})
+                let id = countyDataItem['id']
+                let county = educationData.find((item) => {
+                    return item['fips'] === id
+                })
 
-		  	let percentage = county['bachelorsOrHigher']
+                tooltip.text(county['fips'] + ' - ' + county['area_name'] + ', ' + 
+                    county['state'] + ' : ' + county['bachelorsOrHigher'] + '%')
 
-		  	if (percentage <= 15) {
-		  		return 'red'
-		  	} else if (percentage <= 30) {
-		  		return 'blue'
-		  	} else if (percentage <= 45) {
-		  		return 'green'
-		  	} else {
-		  		return 'gold'
-		  	}
-		  })
-		 .attr('data-fips', (countyDataItem) => {
-		 	return countyDataItem['id']
-		 })
-		 .attr('data-education', (educationDataItem) => {
-		 	let fips = educationDataItem['id']
-		 	let county = educationData.find((county) => {
-		 		return county['fips'] === fips
-		 	})
-		 	let percentage = county['bachelorsOrHigher']
-		 	return percentage
-		 })
-
+                tooltip.attr('data-education', county['bachelorsOrHigher'] )
+            })
+            .on('mouseout', (countyDataItem) => {
+                tooltip.transition()
+                    .style('visibility', 'hidden')
+            })
 }
-
 
 d3.json(countyURL).then(
     (data, error) => {
         if(error){
-            console.log(error)
+            console.log(log)
         }else{
             countyData = topojson.feature(data, data.objects.counties).features
-            console.log('County Data')
             console.log(countyData)
 
             d3.json(educationURL).then(
                 (data, error) => {
                     if(error){
                         console.log(error)
-                    }
-                    else{
+                    }else{
                         educationData = data
-                        console.log('Education Data')
                         console.log(educationData)
                         drawMap()
                     }
-                })
-			}
-    })
-
-
+                }
+            )
+        }
+    }
+)
