1985289fa0481976e28741d564b500cc2c0f9b0a
Gus <gusr@logicliving.com>
2020-07-29 19:42:25 -0500

    pass first 3 tests

---
+++ script.js
@@ -0,0 +1,67 @@
+// console.log(d3)
+// console.log(topojson)
+
+let educationURL =  'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
+let countyURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
+
+let countyData
+let educationData
+
+let canvas = d3.selection('#canvas')
+
+let drawMap = () => {
+
+	canvas.select('path')
+		  .data(countyData)
+		  .enter()
+		  .append('path')
+		  .attr('d', d3.geopath())
+		  .attr('class', 'county')
+		  .attr('fill', (item) => {
+		  	let fips = item['id']
+		  	let county = educationData.find((county) => {
+		  		return county['fips'] === fips
+		  	})
+
+		  	let percentage = county['bachelorsOrHigher']
+
+		  	if (percentage <= 15) {
+		  		return 'red'
+		  	} else if (percentage <= 30) {
+		  		return 'blue'
+		  	} else if (percentage <= 45) {
+		  		return 'green'
+		  	} else {
+		  		return 'gold'
+		  	}
+
+		  })
+
+}
+
+
+d3.json(countyURL).then(
+    (data, error) => {
+        if(error){
+            console.log(error)
+        }else{
+            countyData = topojson.feature(data, data.objects.counties).features
+            console.log('County Data')
+            console.log(countyData)
+
+            d3.json(educationURL).then(
+                (data, error) => {
+                    if(error){
+                        console.log(error)
+                    }
+                    else{
+                        educationData = data
+                        console.log('Education Data')
+                        console.log(educationData)
+                        drawMap()
+                    }
+                })
+			}
+    })
+
+
